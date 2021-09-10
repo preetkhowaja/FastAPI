39da2e2905758f77b35c490b114756b97f600be7
Preet Khowaja <preetkhowaja@gmail.com>
2021-09-10 22:49:19 +0000

    done

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ async def root():
 @app.get("/cal/month")
 async def cal(month: int):
     """Find what classes we have on day input"""
-    c = calendar.TextCalendar(calendar.SUNDAY)
-    print(c.prmonth(2021, month))
+    c = calendar.TextCalendar(calendar.MONDAY)
+    return c
     
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
